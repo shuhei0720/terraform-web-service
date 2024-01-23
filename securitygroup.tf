dde029b37e3c8c4a047f6bafbac6c8de7f3718d1
EC2 Default User <ec2-user@ip-10-0-0-124.ap-northeast-1.compute.internal>
2024-01-23 11:28:15 +0000

    webサーバーのセキュリティグループのルールを追加

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,6 +1,6 @@
-#####################################
+############################################
 #　セキュリティグループの作成
-#####################################
+############################################
 
 # web-a用セキュリティグループ
 
@@ -17,7 +17,7 @@ resource "aws_security_group" "web-service-prod-web-a-securitygroup" {
 
 # web-b用セキュリティグループ
 
-resource "aws_security_group" "web-service-prod-web-b-securitygourp" {
+resource "aws_security_group" "web-service-prod-web-b-securitygroup" {
   name = "web-service-prod-web-b-securitygroup"
   description = "From web-service-prod-alb"
   vpc_id = aws_vpc.vpc.id
@@ -26,4 +26,82 @@ resource "aws_security_group" "web-service-prod-web-b-securitygourp" {
     Project = "web-service",
     Env = "prod"
   }
+}
+
+############################################
+#　セキュリティグループのルールの作成
+############################################
+
+# web-a用セキュリティグループのインバウンドルール 01
+# ALBから80/TCPでリッスンする
+
+resource "aws_security_group_rule" "web-service-prod-web-a-securitygroup-ingress-rule-01" {
+  description = "From web-service-prod-alb"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  type = "ingress"
+  cidr_blocks = ["10.0.100.0/24"]
+  security_group_id = aws_security_group.web-service-prod-web-a-securitygroup.id
+}
+
+# web-a用セキュリティグループのインバウンドルール 02
+# ALBから80/TCPでリッスンする
+
+resource "aws_security_group_rule" "web-service-prod-web-a-securitygroup-ingress-rule-02" {
+  description = "From web-service-prod-alb"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  type = "ingress"
+  cidr_blocks = ["10.0.101.0/24"]
+  security_group_id = aws_security_group.web-service-prod-web-a-securitygroup.id
+}
+
+# web-a用セキュリティグループのアウトバウンドルール 01
+
+resource "aws_security_group_rule" "web-service-prod-web-a-securitygroup-egress-rule-01" {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  type = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web-service-prod-web-a-securitygroup.id
+}
+
+# web-b用セキュリティグループのインバウンドルール 01
+# ALBから80/TCPでリッスンする
+
+resource "aws_security_group_rule" "web-service-prod-web-b-securitygroup-ingress-rule-01" {
+  description = "From web-service-prod-alb"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  type = "ingress"
+  cidr_blocks = ["10.0.100.0/24"]
+  security_group_id = aws_security_group.web-service-prod-web-b-securitygroup.id
+}
+
+# web-b用セキュリティグループのインバウンドルール 02
+# ALBから80/TCPでリッスンする
+
+resource "aws_security_group_rule" "web-service-prod-web-b-securitygroup-ingress-rule-02" {
+  description = "From web-service-prod-alb"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  type = "ingress"
+  cidr_blocks = ["10.0.101.0/24"]
+  security_group_id = aws_security_group.web-service-prod-web-b-securitygroup.id
+}
+
+# web-b用セキュリティグループのアウトバウンドルール 01
+
+resource "aws_security_group_rule" "web-service-prod-web-b-securitygroup-egress-rule-01" {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  type = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web-service-prod-web-b-securitygroup.id
 }
