d17f6e706ef9f361d93c6dca5fe0cd37086efe13
EC2 Default User <ec2-user@ip-10-0-0-124.ap-northeast-1.compute.internal>
2024-01-23 08:41:51 +0000

    s3.tfに、s3バケットACLの設定と、パブリックアクセスブロックの設定を追加

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 ####################################
-#ALB Access Log用S3バケットの作成
+# ALB Access Log用S3バケットの作成
 ####################################
 
 resource "aws_s3_bucket" "web-service-prod-alb-access-log-276229188355" {
@@ -11,3 +11,27 @@ resource "aws_s3_bucket" "web-service-prod-alb-access-log-276229188355" {
     Env = "prod"
   }
 }
+
+###################################
+#　S３バケットのACLの設定
+###################################
+
+# S3バケットのオブジェクト所有者(ACL設定)
+
+resource "aws_s3_bucket_ownership_controls" "web-service-prod-alb-access-log-276229188355-bucket-acl-owner" {
+  bucket = aws_s3_bucket.web-service-prod-alb-access-log-276229188355.id
+  rule { object_ownership = "BucketOwnerEnforced" }
+}
+
+###################################
+#　パブリックアクセスブロック
+###################################
+
+resource "aws_s3_bucket_public_access_block" "web-service-prod-alb-access-log-276229188355-public-access-block" {
+  bucket = aws_s3_bucket.web-service-prod-alb-access-log-276229188355.id
+  # ブロックパブリックアクセスを有効にする
+  block_public_acls = true # S3バケットのパブリックACLをブロックするかどうか
+  block_public_policy = true # S3バケットのパブリックバケットポリシーをブロックするかどうか
+  ignore_public_acls = true # S3バケットのパブリックACLを無視するかどうか
+  restrict_public_buckets = true # S3バケットのパブリックバケットポリシーを制限するかどうか
+} 
