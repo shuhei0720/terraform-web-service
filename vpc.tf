0e5ff0bdb17c22446443a072249f87133944b4a2
EC2 Default User <ec2-user@ip-10-0-0-124.ap-northeast-1.compute.internal>
2024-01-23 08:00:04 +0000

    vpc.tfにて、プライベートサブネット、パブリックサブネットそれぞれのルートを追加

--- vpc.tf
+++ vpc.tf
@@ -136,3 +136,31 @@ resource "aws_route_table" "web-service-prod-ec2-a-subnet-routetable" {
     Env = "prod"
   }
 }
+
+######################################
+# 　　ルートの作成
+######################################
+
+# ALB用パブリックサブネット　アベイラビリティゾーンA用ルートテーブル用ルート01
+
+resource "aws_route" "web-service-prod-alb-a-subnet-route-01" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.web-service-prod-alb-a-subnet-routetable.id
+  gateway_id = aws_internet_gateway.web-service-prod-internetgateway.id
+}
+
+# ALB用パブリックサブネット　アベイラビリティゾーンC用ルートテーブル用ルート01
+
+resource "aws_route" "web-service-prod-alb-c-subnet-route-01" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.web-service-prod-alb-c-subnet-routetable.id
+  gateway_id = aws_internet_gateway.web-service-prod-internetgateway.id
+}
+
+# EC2用プライベートサブネット　アベイラビリティゾーンA用ルートテーブル用ルート01
+
+resource "aws_route" "web-service-prod-ec2-a-subnet-route-01" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.web-service-prod-ec2-a-subnet-routetable.id
+  nat_gateway_id = aws_nat_gateway.web-service-prod-natgateway.id
+}
