6a78f09e8acdcac605361e618f95f1b90365559c
EC2 Default User <ec2-user@ip-10-0-0-124.ap-northeast-1.compute.internal>
2024-01-25 03:56:51 +0000

    alb.tfにweb-a、web-bそれぞれのターゲットグループを作成する記述を追加

--- alb.tf
+++ alb.tf
@@ -25,3 +25,81 @@ resource "aws_lb" "web-service-prod-alb" {
     enabled = true
   }
 }
+
+##########################################################
+#　　ターゲットグループの作成
+##########################################################
+
+# EC2インスタンスweb-a用のターゲットグループ 443→80
+# ターゲットグループ名は32文字以内にする必要がある
+
+resource "aws_lb_target_group" "web-service-prod-web-a-80-tg" {
+  name = "web-service-prod-web-a-80-tg"
+  # ALBからEC2へトラフィックを振り分ける設定
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpc.id
+  # EC2インスタンスを指定する
+  target_type = "instance"
+  slow_start = 0
+
+  # webサーバへの死活監視（ヘルスチェック)設定
+  # Health Checkのmatcherは301も入れる。(HTTPリダイレクトのため)
+  health_check {
+    protocol = "HTTP"
+    path = "/index.html"
+    port = 80
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 30
+    matcher = "200,301"
+  }
+  
+  # ALB構築後にターゲットグループを割り当てる
+  depends_on = [
+    aws_lb.web-service-prod-alb
+  ]
+  tags = {
+    Name = "web-service-prod-web-a-80-tg",
+    Project = "web-service",
+    NewRelicEnv = "prod"
+  }
+}
+
+# EC2インスタンスweb-b用のターゲットグループ 443→80
+# ターゲットグループ名は32文字以内にする必要がある
+
+resource "aws_lb_target_group" "web-service-prod-web-b-80-tg" {
+  name = "web-service-prod-web-b-80-tg"
+  # ALBからEC2へトラフィックを振り分ける設定
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpc.id
+  # EC2インスタンスを指定する
+  target_type = "instance"
+  slow_start = 0
+
+  # webサーバへの死活監視(ヘルスチェック)設定
+  # Health Checkのmasterは301も入れる。(HTTPリダイレクトのため)
+  health_check {
+    protocol = "HTTP"
+    path = "/web-b/index.html"
+    port = 80
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 30
+    matcher = "200,301"
+  }
+
+  # ALB構築後にターゲットグループに割り当てる
+  depends_on = [
+    aws_lb.web-service-prod-alb
+  ]
+  tags = {
+    Name = "web-service-prod-web-b-80-tg",
+    Project = "web-service",
+    NewRelicEnv = "prod"
+  }
+}
